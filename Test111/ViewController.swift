0dd5b4bb5925a740a7d46b4f2e3ba164f11d84ce
Sushil <sushil.kumar10@ibm.com>
2023-01-20 19:32:08 +0530

    Create main branch and 1st commit

--- Test111/ViewController.swift
+++ Test111/ViewController.swift
@@ -9,9 +9,105 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lbl1: UILabel!
+    
+    @IBOutlet weak var lbl2: UILabel!
+    
+    
+    @IBOutlet weak var lbl3: UILabel!
+    
+    @IBOutlet weak var lbl5: UILabel!
+    
+    @IBOutlet weak var lbl4: UILabel!
+    
+    @IBOutlet weak var lbl6: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //UIFont(name: "GillSans-Italic", size: 20) UIFont(name: "GillSans-Italic", size: 20)
+        
+//        self.lbl1.font = UIFont(name: "NewYork-Regular", size: 20)
+//
+//
+//        self.lbl1.font = UIFont(name: "SFProDisplay-Bold", size: 60)
+//
+//        self.lbl2.font = UIFont(name: "SFProDisplay-BoldItalic", size: 60)
+//
+//        self.lbl3.font = UIFont(name: "SFProDisplay-Light", size: 60)
+//
+//        self.lbl4.font = UIFont(name: "SFProDisplay-LightItalic", size: 60)
+//
+//
+//        self.lbl5.font = UIFont(name: "SFProDisplay-Medium", size: 60)
+//
+//        self.lbl6.font = UIFont(name: "SFProDisplay-MediumItalic", size: 60)
+
+        print("\(self.lbl1.font)")
+        print("\(self.lbl2.font)")
+        print("\(self.lbl3.font)")
+        print("\(self.lbl4.font)")
+        print("\(self.lbl5.font)")
+        print("\(self.lbl6.font)")
+//
+        
+//        SF Pro Display
+//        SFProDisplay-Bold
+//        SFProDisplay-BoldItalic
+//        SFProDisplay-Light
+//        SFProDisplay-LightItalic
+//        SFProDisplay-Medium
+//        SFProDisplay-MediumItalic
+//        SFProDisplay-Regular
+//        SFProDisplay-RegularItalic
+//        SFProDisplay-Semibold
+//        SFProDisplay-SemiboldItalic
+        //SFProDisplay-Black
+        
+        
+        
+
+//        <array key="SF-Pro-Display-Bold.otf">
+//            <string>SFProDisplay-Bold</string>
+//        </array>
+//        <array key="SF-Pro-Display-Regular.otf">
+//            <string>SFProDisplay-Regular</string>
+//        </array>
+//        <array key="SF-Pro-Display-Semibold.otf">
+//            <string>SFProDisplay-Semibold</string>
+//        </array>
+//        <array key="SF-Pro-Display-Medium.otf">
+//            <string>SFProDisplay-Medium</string>
+//        </array>
+//        <array key="SF-Pro-Display-RegularItalic.otf">
+//            <string>SFProDisplay-RegularItalic</string>
+//        </array>
+//
+//        <array key="SF-Pro-Display-SemiboldItalic.otf">
+//            <string>SFProDisplay-SemiboldItalic</string>
+//        </array>
+//        
+//        
+//        alpha1.maratin
+        
+        
+        
+        
+        
+        //NewYork-BoldItalic
+        //NewYork-HeavyItalic
+        //NewYork-MediumItalic
+        //NewYork-RegularItalic
+        //NewYork-SemiboldItalic
+        //
+        //NewYork-Regular
+        //NewYork-Medium
+        //NewYork-Bold
+        //NewYork-Semibold
+        //NewYork-Heavy
+        //
+        
     }
 
 
